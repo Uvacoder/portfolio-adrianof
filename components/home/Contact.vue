9f86ca7ebcb7897572a41fc8def979a8d78a1dc5
Leandro Frederico <leandro_frederico@hotmail.com>
2022-09-22 17:05:55 -0300

    chore: Contact refactor

--- components/home/Contact.vue
+++ components/home/Contact.vue
@@ -7,10 +7,11 @@
       <div class="bg-gray-100 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
         <div class="mx-auto max-w-lg h-full flex flex-col">
           <h2 class="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">Contato</h2>
-          <p class="mt-3 text-lg h-full leading-6 text-gray-500">Entre em contato através do email ou WhatsApp, mande suas dúvidas, solicite um orçamento, responderei o mais breve possível.</p>
+          <p class="mt-3 text-lg h-full leading-6 text-gray-500">Entre em contato através do email, WhatsApp ou me ligue e mande
+            suas dúvidas, solicite um orçamento. Responderei o mais breve possível.</p>
           <dl class="mt-8 text-base text-gray-500">
             <div class="mt-6">
-              <dt class="sr-only">Phone number</dt>
+              <dt class="sr-only">Numero de Telefone</dt>
               <dd>
                 <a href="https://wa.me/5511984282844" class="flex" target="_blank">
                   <Icon name="fa-brands:whatsapp" class="h-6 w-6 flex-shrink-0 text-green-400" aria-hidden="true" />
@@ -33,31 +34,19 @@
       </div>
       <div class="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
         <div class="mx-auto max-w-lg lg:max-w-none">
-          <form action="#" method="POST" class="grid grid-cols-1 gap-y-6">
-            <div>
-              <label for="full-name" class="sr-only">Nome completo</label>
-              <input type="text" name="full-name" id="full-name" autocomplete="name"
+          <form action="#" method="POST" class="grid grid-cols-1 gap-y-6" @submit.prevent="">            
+            <div v-for="input in formInputs" :key="input.name">
+              <label :for="input.name" class="sr-only">Message</label>
+
+              <input v-if="input.type === 'text'" :id="input.name" :name="input.name" :type="input.type" :autocomplete="input.autocomplete"
                 class="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                placeholder="Nome completo" />
+                :placeholder="input.placeholder" />
+
+              <textarea v-if="input.type === 'textarea'" :id="input.name" :name="input.name" rows="4"
+              class="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+              :placeholder="input.placeholder" />
             </div>
-            <div>
-              <label for="email" class="sr-only">Email</label>
-              <input id="email" name="email" type="email" autocomplete="email"
-                class="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                placeholder="Email" />
-            </div>
-            <div>
-              <label for="phone" class="sr-only">Telefone</label>
-              <input type="text" name="phone" id="phone" autocomplete="tel"
-                class="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                placeholder="Telefone" />
-            </div>
-            <div>
-              <label for="message" class="sr-only">Message</label>
-              <textarea id="message" name="message" rows="4"
-                class="block w-full rounded-md border-gray-300 py-3 px-4 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-                placeholder="Mensagem" />
-            </div>
+
             <div>
               <button type="submit"
                 class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-3 px-6 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Submit</button>
@@ -70,5 +59,32 @@
 </template>
 
 <script setup>
-// import { EnvelopeIcon, PhoneIcon } from '@heroicons/vue/24/outline'
+
+const formInputs = [
+  {
+    name: 'full-name',
+    placeholder: 'Nome completo',
+    autocomplete: 'name',
+    type: 'text'
+  },
+  {
+    name: 'email',
+    placeholder: 'Email',
+    autocomplete: 'email',
+    type: 'text'
+  },
+  {
+    name: 'phone',
+    placeholder: 'Telefone',
+    autocomplete: 'tel',
+    type: 'text'
+  },
+  {
+    name: 'message',
+    placeholder: 'Mensagem',
+    autocomplete: false,
+    type: 'textarea'
+  }
+]
+
 </script>
