cabab2d88ab825434117c8acff8a888ba5c8d1b9
Leandro Frederico <leandro_frederico@hotmail.com>
2022-09-22 02:08:54 -0300

    feat: created Faq component

---
+++ components/home/Faq.vue
@@ -0,0 +1,60 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div id="faq" class="bg-gray-100">
+    <div class="mx-auto max-w-7xl py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
+      <div class="mx-auto max-w-3xl divide-y-2 divide-gray-200">
+        <h2 class="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Perguntas Frequentes
+        </h2>
+        <dl class="mt-6 space-y-6 divide-y divide-gray-200">
+          <Disclosure as="div" v-for="faq in faqs" :key="faq.question" class="pt-6" v-slot="{ open }">
+            <dt class="text-lg">
+              <DisclosureButton class="flex w-full items-start justify-between text-left text-gray-400">
+                <span class="font-medium text-gray-900">{{ faq.question }}</span>
+                <span class="ml-6 flex h-7 items-center">
+                  <Icon name="heroicons-outline:chevron-down"
+                    :class="[open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform']" aria-hidden="true" />
+                </span>
+              </DisclosureButton>
+            </dt>
+            <transition enter-active-class="duration-150 ease-out" enter-from-class="opacity-0 scale-0"
+              enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in"
+              leave-from-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-0">
+              <DisclosurePanel as="dd" class="mt-2 pr-12">
+                <p class="text-base text-gray-500">{{ faq.answer }}</p>
+              </DisclosurePanel>
+            </transition>
+          </Disclosure>
+        </dl>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
+
+const faqs = [
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+  {
+    question: "What's the best thing about Switzerland?",
+    answer:
+      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+  },
+
+  // More questions...
+]
+</script>
